1069da59aa8114c839ecb0c66fd7e42e5e503cd5
root <root@ip-10-10-10-236.ec2.internal>
2016-04-28 20:41:30 -0400

    security group details applied & utterances added

--- index.js
+++ index.js
@@ -13,49 +13,52 @@ var AlexaSkill = require('./AlexaSkill');
 
 var demo = new AWS.EC2();
 
-//TODO: Populate with stratus-demo values
 var demoParams = {
-  CidrIp: 'STRING_VALUE',
-  DryRun: true || false,
-  FromPort: 0,
-  GroupId: 'STRING_VALUE',
-  GroupName: 'STRING_VALUE',
+//  CidrIp: 'STRING_VALUE',
+//  DryRun: true || false,
+//  FromPort: 0,
+//  GroupId: 'STRING_VALUE',
+//  GroupName: 'STRING_VALUE',
   IpPermissions: [
     {
-      FromPort: 0,
-      IpProtocol: 'STRING_VALUE',
-      IpRanges: [
-        {
-          CidrIp: 'STRING_VALUE'
-        },
+      FromPort: 22,
+      IpProtocol: 'tcp',
+      //IpRanges: [
+      //  {
+      //    CidrIp: 'STRING_VALUE'
+      //  },
         /* more items */
-      ],
-      PrefixListIds: [
-        {
-          PrefixListId: 'STRING_VALUE'
-        },
+      //],
+      //PrefixListIds: [
+      //  {
+      //    PrefixListId: 'STRING_VALUE'
+      //  },
         /* more items */
-      ],
-      ToPort: 0,
+      //],
+      ToPort: 22,
       UserIdGroupPairs: [
         {
-          GroupId: 'STRING_VALUE',
-          GroupName: 'STRING_VALUE',
-          PeeringStatus: 'STRING_VALUE',
-          UserId: 'STRING_VALUE',
-          VpcId: 'STRING_VALUE',
-          VpcPeeringConnectionId: 'STRING_VALUE'
+          GroupId: 'sg-059c807d',
+          //GroupName: 'STRING_VALUE',
+          //PeeringStatus: 'STRING_VALUE',
+          UserId: '517218816149',
+          VpcId: 'vpc-a47b94c3',
+          //VpcPeeringConnectionId: 'STRING_VALUE'
         },
         /* more items */
       ]
     },
     /* more items */
   ],
-  IpProtocol: 'STRING_VALUE',
-  SourceSecurityGroupName: 'STRING_VALUE',
-  SourceSecurityGroupOwnerId: 'STRING_VALUE',
-  ToPort: 0
+  //IpProtocol: 'STRING_VALUE',
+  //SourceSecurityGroupName: 'STRING_VALUE',
+  //SourceSecurityGroupOwnerId: 'STRING_VALUE',
+  //ToPort: 0
 };
+ec2.revokeSecurityGroupIngress(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
 
 // Extend AlexaSkill
 SecurityGuard.prototype = Object.create(AlexaSkill.prototype);
@@ -81,11 +84,15 @@ SecurityGuard.prototype.intentHandlers = {
     "SecurityGuardDrillIntent": function (intent, session, response) {
         response.ask("This is just a drill. If this were an actual emergency, you could ask me to apply an EC2 Security Group to project your EC2 instances from the bad guys.", "");
     }
+    "QueryAlertIntent": function (intent, session, response) {
+	response.ask("Alert! Incoming SSH brute force attack!", "")
+    }
     "DeployCountermeasuresIntent": function (intent, session, response) {
-	demo.authorizeSecurityGroupIngress(params, function(err, data) {
+	demo.revokeSecurityGroupIngress(demoParams, function(err, data) {
   	if (err) console.log(err, err.stack); // an error occurred
   	else     console.log(data);           // successful response
-});
+	response.tell("Applying security counter measures now.");
+    });
     }
     "HelpIntent": function (intent, session, response) {
         response.ask("If you're having trouble, please see the documentation for this Skill for a list of things that you can say to me. If you require more direct assistance, please say, I need professional services", "");
