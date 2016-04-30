9072328c09ace8c5ea5ec623eb12d94b9a218f5e
root <root@ip-10-10-10-236.ec2.internal>
2016-04-29 23:20:59 -0400

    minor edits

--- AlexaSkill.js
+++ AlexaSkill.js
@@ -1,13 +1,22 @@
 /**
     Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
     Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
         http://aws.amazon.com/apache2.0/
+
     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
 
 'use strict';
+
 function AlexaSkill(appId) {
     this._appId = appId;
+}
+
+AlexaSkill.speechOutputType = {
+    PLAIN_TEXT: 'PlainText',
+    SSML: 'SSML'
 }
 
 AlexaSkill.prototype.requestHandlers = {
@@ -26,27 +35,27 @@ AlexaSkill.prototype.requestHandlers = {
 };
 
 /**
- * Override any of the eventHandlers as needed
- */
+ *  * Override any of the eventHandlers as needed
+ *   */
 AlexaSkill.prototype.eventHandlers = {
     /**
-     * Called when the session starts.
-     * Subclasses could have overriden this function to open any necessary resources.
-     */
+ *      * Called when the session starts.
+ *           * Subclasses could have overriden this function to open any necessary resources.
+ *                */
     onSessionStarted: function (sessionStartedRequest, session) {
     },
-
+    
     /**
-     * Called when the user launches the skill without specifying what they want.
-     * The subclass must override this function and provide feedback to the user.
-     */
+ *      * Called when the user invokes the skill without specifying what they want.
+ *           * The subclass must override this function and provide feedback to the user.
+ *                */
     onLaunch: function (launchRequest, session, response) {
         throw "onLaunch should be overriden by subclass";
     },
 
     /**
-     * Called when the user specifies an intent.
-     */
+ *      * Called when the user specifies an intent.
+ *           */
     onIntent: function (intentRequest, session, response) {
         var intent = intentRequest.intent,
             intentName = intentRequest.intent.name,
@@ -60,24 +69,23 @@ AlexaSkill.prototype.eventHandlers = {
     },
 
     /**
-     * Called when the user ends the session.
-     * Subclasses could have overriden this function to close any open resources.
-     */
+ *      * Called when the user ends the session.
+ *           * Subclasses could have overriden this function to close any open resources.
+ *                */
     onSessionEnded: function (sessionEndedRequest, session) {
     }
 };
 
 /**
- * Subclasses should override the intentHandlers with the functions to handle specific intents.
- */
+ *  * Subclasses should override the intentHandlers with the functions to handle specific intents.
+ *   */
 AlexaSkill.prototype.intentHandlers = {};
 
 AlexaSkill.prototype.execute = function (event, context) {
     try {
         console.log("session applicationId: " + event.session.application.applicationId);
-
-        // Validate that this request originated from authorized source.
-        if (this._appId && event.session.application.applicationId !== this._appId) {
+	// Validate that this request originated from authorized source.
+	if (this._appId && event.session.application.applicationId !== this._appId) {
             console.log("The applicationIds don't match : " + event.session.application.applicationId + " and "
                 + this._appId);
             throw "Invalid applicationId";
@@ -92,7 +100,8 @@ AlexaSkill.prototype.execute = function (event, context) {
         }
 
         // Route the request to the proper handler which may have been overriden.
-        var requestHandler = this.requestHandlers[event.request.type];
+        
+	var requestHandler = this.requestHandlers[event.request.type];
         requestHandler.call(this, event, context, new Response(context, event.session));
     } catch (e) {
         console.log("Unexpected exception " + e);
@@ -105,21 +114,29 @@ var Response = function (context, session) {
     this._session = session;
 };
 
+function createSpeechObject(optionsParam) {
+    if (optionsParam && optionsParam.type === 'SSML') {
+        return {
+            type: optionsParam.type,
+            ssml: optionsParam.speech
+        };
+    } else {
+        return {
+            type: optionsParam.type || 'PlainText',
+            text: optionsParam.speech || optionsParam
+        }
+    }
+}
+
 Response.prototype = (function () {
     var buildSpeechletResponse = function (options) {
         var alexaResponse = {
-            outputSpeech: {
-                type: 'PlainText',
-                text: options.output
-            },
+            outputSpeech: createSpeechObject(options.output),
             shouldEndSession: options.shouldEndSession
         };
         if (options.reprompt) {
             alexaResponse.reprompt = {
-                outputSpeech: {
-                    type: 'PlainText',
-                    text: options.reprompt
-                }
+                outputSpeech: createSpeechObject(options.reprompt)
             };
         }
         if (options.cardTitle && options.cardContent) {
@@ -129,9 +146,9 @@ Response.prototype = (function () {
                 content: options.cardContent
             };
         }
-        var returnResult = {
-            version: '1.0',
-            response: alexaResponse
+	var returnResult = {
+                version: '1.0',
+                response: alexaResponse
         };
         if (options.session && options.session.attributes) {
             returnResult.sessionAttributes = options.session.attributes;
@@ -178,3 +195,4 @@ Response.prototype = (function () {
 })();
 
 module.exports = AlexaSkill;
+
